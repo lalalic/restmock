f755dd6b73e1a913522275fa40fceafa5855f4e4
licheng <lalalic@139.com>
2015-06-08 23:05:35 +0800

    fix path

--- browser.js
+++ browser.js
@@ -5,10 +5,10 @@ var conf=require('./mock.json'), //which will be replaced with mock.json in targ
 var _send=XMLHttpRequest.prototype.send
 XMLHttpRequest.prototype.send=function(){
     _send.apply(this,arguments)
-
+    var pathname=(new URL(this.url)).pathname
     for(var i=0, len=routes.length, t; i<len; i++){
         t=routes[i]
-        if(t.path==this.url && t.type==this.method){
+        if(t.path==pathname && t.type.toUpperCase()==this.method){
             var response=t.response,
                 type=(typeof(reponse)=='string' ? 'text/plain' :'application/json');
 
